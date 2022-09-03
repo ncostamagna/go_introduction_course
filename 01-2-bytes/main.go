5ae7bc750b03f674784865e7a018a5cc3383aee1
Nahuel <nlcostamagna@gmail.com>
2022-09-03 15:30:35 -0300

    add bytes file

---
+++ 01-2-bytes/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var A byte = 'A'
+	var a byte = 'a'
+	var R byte = 82
+	var s byte = 115
+
+	fmt.Println()
+	fmt.Println("Value in ASCII Code:")
+	fmt.Println(A)
+	fmt.Println(a)
+	fmt.Println(R)
+	fmt.Println(s)
+
+	fmt.Println()
+	fmt.Println("Value in String:")
+	fmt.Println(string(A))
+	fmt.Println(string(a))
+	fmt.Println(string(R))
+	fmt.Println(string(s))
+}
