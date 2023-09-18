721547bbd0110534ced14d5c539b9e7138b72ffd
Nahuel <nlcostamagna@gmail.com>
2023-09-18 13:25:49 -0300

    binary operations

--- 15-binaryOperators/main.go
+++ 15-binaryOperators/main.go
@@ -8,22 +8,22 @@ func main() {
 	fmt.Printf("Number %d in binary is %b\n", a, a)
 
 	fmt.Printf("\nLeft shift\n")
-	fmt.Printf("Number %d in binary is %b\n", a << 1, a << 1)
-	fmt.Printf("Number %d in binary is %b\n", a << 10, a << 10)
+	fmt.Printf("Number %d in binary is %b\n", a<<1, a<<1)
+	fmt.Printf("Number %d in binary is %b\n", a<<10, a<<10)
 
 	fmt.Printf("\nRight shift\n")
-	fmt.Printf("Number %d in binary is %b\n", a >> 1, a >> 1)
-	fmt.Printf("Number %d in binary is %b\n", a >> 10, a >> 10)
-	fmt.Printf("Number %d in binary is %b\n", a >> 5, a >> 5)
+	fmt.Printf("Number %d in binary is %b\n", a>>1, a>>1)
+	fmt.Printf("Number %d in binary is %b\n", a>>10, a>>10)
+	fmt.Printf("Number %d in binary is %b\n", a>>5, a>>5)
 
 	fmt.Printf("\n'a' and 'b' value:\n")
 	var b uint16 = 20
 	fmt.Printf("'a': %.3d - %.10b\n", a, a)
 	fmt.Printf("'b': %.3d - %.10b\n\n", b, b)
 
-	fmt.Printf("Bitwise AND: %d - %.10b\n", a & b, a & b)
-	fmt.Printf("Bitwise OR: %d - %.10b\n", a | b, a | b)
-	fmt.Printf("Bitwise XOR: %d - %.10b\n", a ^ b, a ^ b)
+	fmt.Printf("Bitwise AND: %d - %.10b\n", a&b, a&b)
+	fmt.Printf("Bitwise OR: %d - %.10b\n", a|b, a|b)
+	fmt.Printf("Bitwise XOR: %d - %.10b\n", a^b, a^b)
 	fmt.Printf("Bitwise NOT: %d - %.10b\n", ^a, ^a)
 
 	fmt.Println()
@@ -31,18 +31,24 @@ func main() {
 	fmt.Printf("Bitwise NOR: %d - %.10b\n", ^(a | b), ^(a | b))
 	fmt.Printf("Bitwise XNOR: %d - %.10b\n", ^(a ^ b), ^(a ^ b))
 
-	rol1 := 1
-	rol2 := 1 << 1
-	rol3 := 1 << 2
-	rol4 := 1 << 3
+	READ_ROLE := 1        //0001
+	WRITE_ROLE := 1 << 1  //0010
+	UPDATE_ROLE := 1 << 2 //0100
+	DELETE_ROLE := 1 << 3 //1000
+
 	fmt.Println()
-	fmt.Printf("Roles Binary: %.4b, %.4b, %.4b & %.4b \n", rol1, rol2, rol3, rol4)
-	fmt.Printf("Roles Number: %.4d, %.4d, %.4d & %.4d \n", rol1, rol2, rol3, rol4)
-	
-	myProfile := rol1 + rol4
+	fmt.Printf("Roles Binary: %.4b, %.4b, %.4b & %.4b \n", READ_ROLE, WRITE_ROLE, UPDATE_ROLE, DELETE_ROLE)
+	fmt.Printf("Roles Number: %.4d, %.4d, %.4d & %.4d \n", READ_ROLE, WRITE_ROLE, UPDATE_ROLE, DELETE_ROLE)
+
+	myProfile := READ_ROLE + WRITE_ROLE + DELETE_ROLE
 	fmt.Printf("My profile: (%d, %b) \n", myProfile, myProfile)
 
-		
 	fmt.Println()
-	fmt.Println(0 != (myProfile & rol1))
+
+	fmt.Println("Does the user have permissions to perform the operation?")
+	if (myProfile & DELETE_ROLE) != 0 {
+		fmt.Println("Yes! :D")
+	} else {
+		fmt.Println("No :(")
+	}
 }
